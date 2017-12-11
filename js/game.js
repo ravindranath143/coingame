07fa7367cb569b6289ebca6d6cfb3bf286adfe4b
adeykrishna <adeykrishna@gmail.com>
2017-12-11 12:47:22 +0530

    chnages

--- js/game.js
+++ js/game.js
@@ -30,6 +30,7 @@
                 if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
                     is_mobile = !0;
                 }else{
+                    
                     is_mobile = 0;
                 }
                 startscreen.style.display = 'block';
